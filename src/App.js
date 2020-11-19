60f0b58438cce377b4ae48bcbd04489b08870a5f
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2020-11-19 19:45:05 +0500

    Create Queen Elice element
    1:- Added Queen image in the images folder
    2:- Added earth image in the images folder
    2:- Created QueenElice.js to create QueenElice element
    3:- Updated Queenelice.js to add background for earth and Queen Image
    4:- Created QueebElice.css to update styling
    5:- Updated QueenElice.js to add animation and update animation speed based upon clicks and time
    6:- Updated app.js to add Queenelice Element

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 // Imports
+// React Imports
+import React, {useState} from 'react';
 // Component Imports
 import { Sky } from './components/sky/Sky';
+import {EliceQueen} from './components/eliceQueen/EliceQueen';
 //Styles Imports
 import './App.css';
 
 // App Function
 function App() {
+  const [increaseSpeed, setIncreaseSpeed] = useState(0);
   return (
-    <div>
-      <Sky></Sky>
+    <div onClick={() => setIncreaseSpeed(increaseSpeed+1)}>
+      <Sky/>
+      <EliceQueen value = {increaseSpeed}/>
     </div>
   );
 }
---
+++ src/components/eliceQueen/EliceQueen.js
@@ -0,0 +1,67 @@
+// imports
+// React Imports
+import React,{useState, useEffect} from 'react';
+// Images Imports
+import sprite_running_alice_queen_small from '../../images/sprite_running-alice-queen_small.png';
+// WebAnimations Imports
+import useWebAnimations from "@wellyshen/use-web-animations";
+// Styles Imports
+import './EliceQueen.css';
+
+export const EliceQueen = (props) => {
+
+    // usestate for using condition for speedup
+    const [speedUp, setSpeedUp] = useState(props.value);
+
+    // Update value of speed up when onclick Functions execute to change value of props
+    useEffect(() => {
+        return () => {
+            setSpeedUp(props.value)
+        }
+    }, [props.value])
+
+    // WebAnimations definantions
+    const { ref, playState, getAnimation } = useWebAnimations({
+        keyframes:[
+            { transform: 'translateY(0)' },
+            { transform: 'translateY(-100%)' }   
+        ],
+        timing: { 
+        easing: 'steps(7, end)',
+        direction: "reverse",
+        duration: 400,
+        playbackRate: 1,
+        iterations: Infinity
+        }
+    })
+
+    // check if animations is runnig or not and update playback rate
+    if(playState==="running"){
+        var animation = getAnimation();
+        //Increase playback rate based upon click
+        if (speedUp > 0){
+                animation.updatePlaybackRate(animation.playbackRate += 0.5);
+            setInterval(() => {
+                setSpeedUp(0)
+            }, 3000);
+        }else{
+            //Decrease playback rate after particular interval
+            setInterval( function () {
+                if (animation.playbackRate > 0.4) {
+                    animation.updatePlaybackRate(animation.playbackRate -= 0.1);
+                    console.log(animation.playbackRate)   
+                }
+            },10000)
+        }
+    }
+
+  return (
+    <div>
+      <div className="earth">
+        <div className="alice">
+            <img ref={ref} src={sprite_running_alice_queen_small} alt=" " className="aliceImag"/>
+        </div>
+      </div>
+    </div>
+  );
+}
